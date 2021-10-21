b8490bcb53d92c269edfc5739cf5eb67c0cbead3
RidhoFauziGrafika <92265091+RidhoFauziGrafika@users.noreply.github.com>
2021-10-21 21:30:12 +0700

    Add files via upload

---
+++ pertemuan2.py
@@ -0,0 +1,44 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> namaSiswa = "Ridho Fauzi Grafika"
+>>> gender = "L"
+>>> umur = 19
+>>> berat_badan = 50.0
+>>> print("Nama Siswa:",namaSiswa,
+      "\nJenis Kelamin:",gender,
+      "\nUmur:",umur,"tahun",
+      "\nBerat Badan:",berat_badan,"kg")
+Nama Siswa: Ridho Fauzi Grafika 
+Jenis Kelamin: L 
+Umur: 19 tahun 
+Berat Badan: 50.0 kg
+>>> 
+>>> jari2 = 10
+>>> luas = 3.14 * jari2 * jari2
+>>> keliling = 2 * 3.14 * jari2
+>>> kursus = "Pemrograman Python Fundamental"
+>>> lembaga = "Nurul Fikri Computer"
+>>> title = kursus + lembaga
+>>> print (title,"\nkeliling lingkaran dengan jari jari",jari2,"=",keliling,
+       "\ndan Luas =",luas)
+Pemrograman Python FundamentalNurul Fikri Computer 
+keliling lingkaran dengan jari jari 10 = 62.800000000000004 
+dan Luas = 314.0
+>>> 
+>>> sisi = 5
+>>> luas = sisi * sisi
+>>> keliling = 4 * sisi
+>>> print("luas persegi adalah =",luas,
+      "\nkeliling persegi adalah =",keliling)
+luas persegi adalah = 25 
+keliling persegi adalah = 20
+>>> 
+>>> panjang = 5
+>>> lebar = 5
+>>> luas = panjang * lebar
+>>> keliling = 2 *(panjang + lebar)
+>>> print("luas persegi panjang adalah =",luas,
+      "\nkeliling persegi panjang adalah =",keliling)
+luas persegi panjang adalah = 25 
+keliling persegi panjang adalah = 20
+>>> 
