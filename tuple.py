98425d8fc27794a438b95f536a2d8611b35e8000
RidhoFauziGrafika <92265091+RidhoFauziGrafika@users.noreply.github.com>
2021-11-01 16:25:24 +0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,8 @@
+tup1 = ('PKI 1', 'PKI 2', 1948, 1965)
+tup2 = (1, 2, 3, 4, 5)
+tup3 = ("Kucing", "Ikan cupang", "Kelinci", "Burung", "Angsa")
+
+print("G30S/PKI terjadi pada tahun", tup1[3])
+print("Bilangan Index Antara 1 dan 5 adalah", tup2[1:5])
+print("Binatang kesayanganku adalah", tup3[0])
+print("Jumlah hewan peliharaanku ada", len(tup3))
